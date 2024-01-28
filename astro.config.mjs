c4f5577856bf828c289b979565be16c9b4f0152d
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2024-01-28 21:56:55 +0100

    First website design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), icon({
+      include: {
+          mdi: ["*"]
+      }
+  })],
   site: "https://www-new.rogama25.es"
 });
